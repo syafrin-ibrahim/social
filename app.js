a1b8df8511bd4cb8ad86c650f5ff76c7983ecc04
syafrin <syafrinibrahim12@gmail.com>
2020-04-24 12:23:23 -0700

    set up app, konek db, set routing

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const connect  = require('./config/db');
+
+
+connect();
+
+app.get('/',(req, res)=>{
+    res.json('api is work hello world');
+})
+
+// define routing
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+    console.log(`listen on port ${PORT}`);
+})
---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('dbUrl');
+
+//koneksi db
+
+const connectDb = async ()=>{
+    try{
+        await mongoose.connect(db, {useNewUrlParser : true});
+        console.log('mongo db connected..');
+    }catch(e){
+        console.log(e.message);
+        
+        //exit proses failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDb;
