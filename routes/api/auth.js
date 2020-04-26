1575ab22e8d35720d9318c028490ccad1bd6e076
syafrin <syafrinibrahim12@gmail.com>
2020-04-26 13:27:41 -0700

    get all profile and profile Bu Id user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,16 +2,76 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const { check, validationResult} = require('express-validator');
 
+//@route get /auth
+//@desc auth User
+//@access Public
 router.get('/', auth,  async (req, res)=> {
     // res.send('Auth Routing')
     try{
-        const user = await User.findById(req.user.id).select('-passeord');
+        const user = await User.findById(req.user.id).select('-password');
         res.json(user);
     }catch(err){
         console.log(err);
         res.status(401).json({ msg : 'gagal'});
     }
 });
+
+//@route post api/auth
+//@desc Login User
+//@access Public
+router.post('/',[
+  
+    check('email', 'Email harus valid').isEmail(),
+    check('password', 'Password harus 6 karakter').exists()
+],async (req, res)=>{
+    //handle validasi error 
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors : errors.array()});
+    }
+    // membuat variabel destructring 
+    const { email, password} = req.body;
+    try{
+            let user =  await User.findOne({ email });
+            //jika user tidak ada
+            if(!user){
+                return res.status(400).json({ error : [{ msg : "User Invalid"}]});
+            }
+
+           //compare password
+            const isMatch = await bcrypt.compare(password, user.password);
+            if(!isMatch){
+                return res.status(400).json({ error : [{ msg : "Password Invalid"}]});    
+            }
+          
+            //return jsonwebtoken
+            const payload = {
+                user : {
+                    id : user.id
+                }
+            };
+            jwt.sign(payload, config.get('jwt-secret'),{expiresIn : 36000},
+                (err, token)=>{
+                    if(err){
+                        throw err;
+                    }
+
+                    res.json({token});
+                }
+            )
+            //res.send('User Routing');
+    }catch(e){
+            console.log(e.message);
+            res.status(500).send("server error");
+    }
+ 
+});
+
+
 
 module.exports = router; 
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,121 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const { check, validationResult} = require('express-validator');
 
-router.get('/', (req, res)=> res.send('Profile Routing'));
+//@route post api/profile/me
+//@desc get Profile User
+//@access Private
+router.get('/me', auth, async (req, res)=>{;
+    try{
+        const profile = await Profile.findOne({ user : req.user.id }).populate('user',['name', 'avatar']);
+        if(!profile){
+            res.status(400).json({ msg : 'tidak ada profile untuk user ini'});
+        }
+
+        res.status(200).json(profile);
+    }catch(err){
+        console.log(err.message); 
+        res.status(500).send('server error');       
+    }
+});
+
+//@route post api/profile
+//@desc create or update profile
+//@access Private
+router.post('/', [auth, [
+    check('status', 'status harus diisi').not().isEmpty(),
+    check('skills', 'skills harus diisi').not().isEmpty(),
+]], async (req, res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors : errors.array()});
+    }
+
+    const {
+         bio,status, location,
+         website, company, skills,
+         githubusername, youtube,facebook,
+         twitter, instagram,linkedin} = req.body;
+
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if(company)profileFields.company = company;
+    if(website)profileFields.website = website;
+    if(location)profileFields.location = location;
+    if(bio)profileFields.bio=bio;
+    if(status)profileFields.status=status;
+    if(bio)profileFields.bio=bio;
+    if(githubusername)profileFields.githubusername=githubusername;
+    if(skills){
+            profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
+    // console.log(profileFields.skills);
+    // res.send(profileFields.skills);
+
+    profileFields.social = {};
+    if(youtube)profileFields.social.youtube = youtube;
+    if(facebook)profileFields.social.facebook = facebook;
+    if(twitter)profileFields.social.twitter = twitter;
+    if(instagram)profileFields.social.instagram = instagram;
+    if(linkedin)profileFields.social.linkedin = linkedin;
+
+    try{
+        //cek jika ada profile di database
+        let profile = await Profile.findOne({ user : req.user.id});
+        //jika sudah ada profile
+        if(profile){
+            profile = await Profile.findOneAndUpdate({ user : req.user.id},{ $set : profileFields},{ new : true});
+
+            return res.json(profile);
+        }
+
+        //jika belum ada buat profile baru
+        profile = new Profile(profileFields);
+        await profile.save();
+        res.json(profile);
+    }catch(err){
+        console.log(err.message);
+        res.status(500).send("server error");
+    }
+
+})
+
+
+//@route get api/profile
+//@desc get all profile
+//@access Public
+
+router.get('/', async (req, res)=>{
+    try{
+        const profiles = await Profile.find().populate('User',['name','avatar']);
+        res.status(200).json(profiles);
+    }catch(err){
+        console.log(err.message);
+        res.status(500).send('server error');
+    }
+})
+
+//@route get api/profile/user/user_id
+//@desc get profile by id
+//@access Public
+router.get('/user/:user_id', async (req, res)=>{
+    try{
+        const profile = await Profile.find({ user : req.params.user_id }).populate('User',['name','avatar']);
+        if(!profile){
+            return res.status(400).json({ msg :  'profile tidak ditemukan'})
+        }
+        res.status(200).json(profile);
+    }catch(err){
+        if(err.kind == 'ObjectId'){
+            return res.status(400).json({ msg :  'profile tidak ditemukan'})
+        }
+
+       // console.log(err.message);
+        res.status(500).send('server error');
+    }
+})
 
 module.exports = router; 
