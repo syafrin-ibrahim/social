ea1c34928bdd5b224336c971ed2723ecf483a7e9
syafrin <syafrinibrahim12@gmail.com>
2020-05-07 14:23:36 -0700

    setting production

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const connect  = require('./config/db');
-
+//configurasi production
+const path = require('path');
 
 connect();
 //initial middleware
 app.use(express.json({extended : false}));
-app.get('/',(req, res)=>{
-    res.json('api is work hello world');
-})
+// app.get('/',(req, res)=>{
+//     res.json('api is work hello world');
+// })
 
 // define routing
 app.use('/api/users', require('./routes/api/users'));
@@ -16,6 +17,14 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
+//configurasi production
+if (process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+   
+    app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+ })
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
